bf30d057a6c255819acf9c6637b52f2be0b4d64e
Ophek Alon <ophekal@std.hac.ac.il>
2024-02-14 17:07:45 +0200

    17:07

--- include/Button.h
+++ include/Button.h
@@ -5,11 +5,13 @@
 class Button
 {
 public:
-	Button() = default;
-	void printButton(sf::RenderWindow& window, sf::Font& font, std::string type) const;
+	Button();
+	void updateButton(const sf::Font& font, std::string type, float x, float y);
+	void printButton(sf::RenderWindow& window) const;
 
 private:
 	sf::Text m_text;
-	sf::RectangleShape m_button;
+	sf::RectangleShape m_rectangle;
+
 	//sf::Sound m_buttonSound;	//the sound when a button is pressed
 };
--- src/Button.cpp
+++ src/Button.cpp
@@ -4,17 +4,26 @@
 
 
 Button::Button()
+    :m_rectangle(sf::Vector2f(200.f, 100.f))
 {
 }
 
-void Button::printButton(sf::RenderWindow& window, sf::Font &font, std::string type ) const
+void Button::updateButton(const sf::Font& font, std::string type, float x, float y)
 {
-    m_text.setFont(font)
+    //Set the position and color of the rectangle
+    m_rectangle.setPosition(x,y);
+    m_rectangle.setFillColor(sf::Color::White);
+    m_text.setFont(font);
     m_text.setString(type);
-    text.setCharacterSize(24);
-    text.setFillColor(sf::Color::Black);
+    m_text.setCharacterSize(24);
+    m_text.setFillColor(sf::Color::Black);
 
-    // Set the position of the text (relative to the window)
-    text.setPosition(100, 100);
+    // Set the position of the text (relative to the rectangle)
+    m_text.setPosition(130.f, 120.f);
 
+}
+
+void Button::printButton(sf::RenderWindow& window) const
+{
+    window.draw(m_text);
 }

e1dad5c1c86ee65532f71172af65debd64a6931b
Ophek Alon <ophekal@std.hac.ac.il>
2024-02-14 15:35:00 +0200

    hi

--- include/Controller.h
+++ include/Controller.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <memory>
+#include "MovingObjects.h"
+#include "Board.h"
+#include "InfoBar.h"
 
 class Controller
 {
@@ -17,6 +20,7 @@ private:
 	int m_levelNum;	//saves the level number
 
 	std::vector<std::unique_ptr<MovingObjects>> m_movingObjects;
+	std::vector<std::string> m_playlist;
 
 	//Mouse m_mouse;
 	//std::vector<std::unique_ptr<Cat>> m_cats;

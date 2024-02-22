d2c687dd0d880575227a1018ee0da399e7d28e14
Ophek Alon <ophekal@std.hac.ac.il>
2024-02-22 09:14:32 +0200

    9:14

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -89,6 +89,7 @@ void Controller::startGame(sf::RenderWindow& window)
 //-----------------------------------------------------------------
 void Controller::checkMovingObjectCollision(const std::unique_ptr<MovingObjects>& object)
 {
+	//checking if collided with cats
 	for (auto index = 0; index < m_cats.size(); index++)
 	{
 		if (object->checkCollision(*m_cats[index]))
@@ -98,6 +99,7 @@ void Controller::checkMovingObjectCollision(const std::unique_ptr<MovingObjects>
 		}
 	}
 
+	//checking if collided with mouse
 	if (object->checkCollision(*m_mouse))
 	{
 		object->collisionHandling(*m_mouse);
